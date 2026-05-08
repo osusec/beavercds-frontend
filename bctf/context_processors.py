4960653e412f7fb7e82ab7431452f30af1ae0b4b
Casey Colley <code@caseycolley.com>
2026-05-08 00:02:20 -0700

    add front page socials

--- bctf/context_processors.py
+++ bctf/context_processors.py
@@ -6,6 +6,10 @@ def ctf_event (request):
         "event_start": settings.CTF_EVENT_START,
         "event_end": settings.CTF_EVENT_END,
         "ctftime_link": settings.CTF_CTFTIME_LINK,
+        "gitlab_link": settings.CTF_GITLAB_LINK,
+        "discord_link": settings.CTF_DISCORD_LINK,
+        "email_link": settings.CTF_EMAIL_LINK,
+        "bluesky_link": settings.CTF_BSKY_LINK,
         'sponsors': settings.SPONSORS
     }
     
