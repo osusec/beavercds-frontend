d62f7529a2df295ade5d704ecb6a4dc88a688282
Casey Colley <code@caseycolley.com>
2026-08-19 22:09:34 -0700

    add brackets

--- account/models.py
+++ account/models.py
@@ -9,11 +9,20 @@ from bctf.settings import TOKEN_LENGTH
 
 # Create your models here.
 
+class CTFTeam_Bracket (models.Model):
+    bracket_name = models.CharField(unique=True, max_length=200)
+    open_bracket = models.BooleanField(default=True)
+    access_hash = models.CharField(max_length=128, null=True)
+    # 128 is chosen because thats what django's auth helper code sets
+    # this field being null means it is an open bracket
+
+
 class CTFTeam (AbstractBaseUser, PermissionsMixin):
     # TODO: no delineation between CTFTime and traditional logins
     # Team name = 128 characters to match CTFTime
     team_name = models.CharField (primary_key=True, max_length=128)
     ctftime_bool = models.BooleanField()
+    bracket = models.ForeignKey(CTFTeam_Bracket, on_delete=models.PROTECT, null=True)
     username = None
     is_admin = models.BooleanField(default=False) # TODO
 
--- bctf-api/urls.py
+++ bctf-api/urls.py
@@ -6,6 +6,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     # API endpoints below
+    # Note the lack of trailing slashes in contrast to their rest of the website;
+    #  this is following API convention
     path ('api/checkaccess', CheckAccess.as_view(), name='check-access'),
     path ('api/resolvestate', ResolveState.as_view(), name='resolve-state'),
 ]
