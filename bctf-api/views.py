17b32584e05bf23c39713de597217c55faba0f53
Casey Colley <code@caseycolley.com>
2026-08-21 20:25:41 -0700

    use API to update brackets

--- bctf-api/views.py
+++ bctf-api/views.py
@@ -6,6 +6,7 @@ from django.db.models.functions import Rank
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound, JsonResponse
 from django.db import transaction
 from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
+from django.contrib.auth.hashers import make_password, check_password
 from django.views import View
 import json
 
@@ -26,7 +27,7 @@ class ResolveState(View):
         # If not, fail early
         if not _check_unique_ids(new_json_state):
             return JsonResponse("Duplicate IDs present.", status=400)
-        if not _check_complete_fields(new_json_state):
+        if not _check_complete_chal_fields(new_json_state):
             return JsonResponse("Missing required fields.", status=400)
 
         result_state = []
@@ -108,6 +109,76 @@ class ResolveState(View):
         return JsonResponse({"current": result_state, "removed": removed_state})
 
 
+class UpdateBrackets(View):
+    # This function is a lot less heavy than ResolveState because it is
+    #  assumed that there are a lot less brackets than challenges and
+    #  that brackets will change much less frequently than challenges
+    def post (self, request):
+        try:
+            new_json_state = json.loads(request.body.decode('utf-8'))
+        except json.decoder.JSONDecodeError:
+            return JsonResponse("Missing or bad JSON provided.", status=400)
+
+        if not _check_unique_brackets(new_json_state):
+            return JsonResponse("Duplicate bracket names present.", status=400)
+        if not _check_complete_bracket_fields(new_json_state):
+            return JsonResponse("Missing required fields.", status=400)
+
+        result_state = []
+        current_state = set(CTFTeam_Bracket.objects.values_list('bracket_name', flat=True))
+
+        # Begin changing state
+        with transaction.atomic():
+            for bracket_json in new_json_state:
+                bracket_name = bracket_json["name"]
+                open_bracket = False if bracket_json["password"] else True
+                access_hash = None if open_bracket else make_password(bracket_json["password"])
+
+                if bracket_name in current_state:
+                    # Currently existing bracket
+                    (
+                        CTFTeam_Bracket.objects
+                        .filter(bracket_name=bracket_name)
+                        .update(
+                            open_bracket=open_bracket,
+                            access_hash=access_hash
+                        )
+                    )
+                    current_state.remove(bracket_name)
+                else:
+                    # New bracket
+                    new_bracket = CTFTeam_Bracket(
+                        bracket_name=bracket_name,
+                        open_bracket=open_bracket,
+                        access_hash=access_hash
+                    )
+                    new_bracket.save()
+                result_state.append(bracket_name)
+
+            # Lock the users table to edit brackets
+            for i in CTFTeam.objects.select_for_update().all():
+                break
+
+            # Remove what's left
+            for old_bracket in current_state:
+                # Remove teams from the bracket if need be
+                # This places them in the default Open bracket
+                (
+                    CTFTeam.objects
+                    .filter(bracket__bracket_name=old_bracket)
+                    .update(bracket=None)
+                )
+                # Delete the bracket
+                (
+                    CTFTeam_Bracket.objects
+                    .filter(bracket_name=old_bracket)
+                    .delete()
+                )
+        # end: with transaction.atomic()
+
+        return JsonResponse({"current": result_state, "removed": list(current_state)})
+
+
 def _check_unique_ids (new_chal_state):
     # Check that all ids are unique
     new_ids = set()
@@ -118,7 +189,7 @@ def _check_unique_ids (new_chal_state):
         new_ids.add(c['id'])
     return True
 
-def _check_complete_fields (new_chal_state):
+def _check_complete_chal_fields (new_chal_state):
     for c in new_chal_state:
         if ((not "id" in c) or 
         (not "name" in c) or
@@ -128,6 +199,23 @@ def _check_complete_fields (new_chal_state):
         (not "min_points" in c) or
         (not "max_points" in c)  or
         (not "flag" in c)):
+            # Missing required fields, fail
+            return False
+    return True
+
+def _check_unique_brackets (new_bracket_state):
+    # Check that all names are unique
+    new_names = set()
+    for b in new_bracket_state:
+        if b['name'] in new_names:
+            # Duplicate name detected, fail
+            return False
+        new_names.add(b['name'])
+    return True
+
+def _check_complete_bracket_fields (new_bracket_state):
+    for b in new_bracket_state:
+        if (not "name" in b) or (not "password" in b):
             # Missing required fields, fail
             return False
     return True
