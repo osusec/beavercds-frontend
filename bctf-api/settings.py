97c4dc47cbe3bb7115ad4a57c4236fab958f21d0
Casey Colley <code@caseycolley.com>
2026-08-21 20:25:41 -0700

    use API to update brackets

--- bctf-api/settings.py
+++ bctf-api/settings.py
@@ -71,7 +71,12 @@ DATABASES = {
     }
 }
 
-
+# Needed to create valid bracket password hashes
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
